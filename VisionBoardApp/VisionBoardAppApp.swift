9e8cdac9da5abcd100cd8aba77046f20457ee87e
SamB <farflenation@gmail.com>
2025-07-23 22:13:49 -0700

    Initial Commit

---
+++ VisionBoardApp/VisionBoardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionBoardAppApp.swift
+//  VisionBoardApp
+//
+//  Created by Sam Beals on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct VisionBoardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisionBoardAppTests/VisionBoardAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionBoardAppTests.swift
+//  VisionBoardAppTests
+//
+//  Created by Sam Beals on 7/23/25.
+//
+
+import Testing
+@testable import VisionBoardApp
+
+struct VisionBoardAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
